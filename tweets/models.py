6d3a3837f5158de97eb4d7f7018c6eed6a29bc49
grv20 <windroid20@gmail.com>
2020-10-06 16:04:07 +0530

    Handling errors in vanilla js

--- tweets/models.py
+++ tweets/models.py
@@ -9,5 +9,16 @@ class Tweet(models.Model):
     image = models.FileField(upload_to='images/', blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)\
 
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0,200),
+            "time": self.date_created
+        }
+
     def __str__(self):
         return self.content
--- tweets/views.py
+++ tweets/views.py
@@ -2,8 +2,6 @@ import random
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import  is_safe_url
-from datetime import datetime
-from django.db.models.functions import ExtractYear
 from django.conf import settings
 from .forms import TweetForm
 from .models import Tweet
@@ -21,8 +19,7 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content":x.content,
-    "time":x.date_created,"likes":random.randint(0,122)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
@@ -30,7 +27,6 @@ def tweet_list_view(request, *args, **kwargs):
     return JsonResponse(data)
 
 def tweet_create_view(request, *args, **kwargs):
-    print("ajax",request.is_ajax())
     form = TweetForm(request.POST or None) #TweetForm class can be initialized with data or not
     next_url = request.POST.get("next") or None
     if form.is_valid(): #if form is invalid page will render invalid form
@@ -38,10 +34,13 @@ def tweet_create_view(request, *args, **kwargs):
         # do other form related logic
         obj.save()
         if request.is_ajax():
-            return JsonResponse({}, status=201) #201==created items
+            return JsonResponse(obj.serialize(), status=201) #201==created items
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm() #re-initialize blank form
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form":form})
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
