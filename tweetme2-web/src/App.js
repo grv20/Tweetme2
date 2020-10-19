6894a39e256623f14b3e87d821211d2d46c282b0
grv20 <windroid20@gmail.com>
2020-10-19 16:36:40 +0530

    Before handling CORS

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function loadTweets(callback){
+  const xhr = new XMLHttpRequest() //retrieval of data  from xhr for
+  //purpose of continually modifying loaded web page underlying concept
+  //of AJAX Design.
+  const method = 'GET'
+  const url = "http://127.0.0.1:8000/api/tweets/"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method,url) //open up this method with this url
+  xhr.onload = function(){
+    callback(xhr.response,xhr.status)
+}
+xhr.onerror = function (e){
+  console.log(e)
+  callback({"message": "The request was an error!"}, 400)
+}
+xhr.send() //trigger the request after i send it up
+
+}
+
+
+
 function App() {
+  const [tweets, setTweets] = useState([])
+  
+
+  useEffect(() =>
+  {
+    const myCallback = (response, status) => {
+      console.log(response,status)
+      if(status === 200){
+        setTweets(response)
+
+      }
+      else{
+        alert("There was an error")
+      }
+      
+    }
+    // do my lookup
+    loadTweets(myCallback);
+    
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet,index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
