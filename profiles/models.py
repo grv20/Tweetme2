069863b6bd31f2c68cef76bac3b6ade77cbaf2bd
grv20 <windroid20@gmail.com>
2020-10-31 17:32:34 +0530

    Signals to Create Profile Objects

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
@@ -8,3 +8,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+
+    def user_did_save(sender, instance, created, *args, **kwargs):
+    #Profile.objects.get_or_create(user=instance)
+        if created:
+            Profile.objects.get_or_create(user=instance)
+
+    post_save.connect(user_did_save, sender=User)
+#first arg is reciever function name 
+#second arg is which model is it
+#so it will trigger user every single time
+#instance is model
